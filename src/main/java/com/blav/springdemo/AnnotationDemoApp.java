55c3ac71edf034c2043aded923beb14c762cb0fd
mdziuba <maciejdziuba92@gmail.com>
2022-12-05 13:20:57 +0100

    autowired all on fields * added values from properties file

--- src/main/java/com/blav/springdemo/AnnotationDemoApp.java
+++ src/main/java/com/blav/springdemo/AnnotationDemoApp.java
@@ -14,6 +14,15 @@ public class AnnotationDemoApp {
         Coach theCoach = context.getBean("tennisCoach", Coach.class);
         // call methods on bean
         System.out.println(theCoach.getDailyWorkout());
+        System.out.println(theCoach.getDailyFortune());
+
+        SwimCoach coach2 = context.getBean("swimCoach", SwimCoach.class);
+        System.out.println("\n" + coach2.getDailyFortune());
+        System.out.println(coach2.getDailyWorkout());
+        System.out.println("email: " + coach2.getEmail());
+        System.out.println("Team: " + coach2.getTeam());
+
+
         //close container
         context.close();
     }
--- src/main/java/com/blav/springdemo/TennisCoach.java
+++ src/main/java/com/blav/springdemo/TennisCoach.java
@@ -1,13 +1,46 @@
 package com.blav.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach  implements Coach{
+//    @Autowired
+//    @Qualifier("randomFortuneService")
+    private FortuneService fortuneService;
 
+    @Autowired
+    public TennisCoach (
+                            @Qualifier("randomFortuneService")
+                            FortuneService fortuneService )
+    {
+        this.fortuneService = fortuneService;
+        System.out.println(">>Inside default constructor");
+    }
+    /*
+    @Autowired
+    public void doSomeCreazyStuff(FortuneService fortuneService){
+        this.fortuneService=fortuneService;
+    }
+     */
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
 
     @Override
     public String getDailyWorkout() {
         return "Practice your backhand volley";
     }
+
+//    public FortuneService getFortuneService() {
+//        return fortuneService;
+//    }
+//    @Autowired
+//    public void setFortuneService(FortuneService fortuneService) {
+//        System.out.println(">>isnide autowired setter");
+//        this.fortuneService = fortuneService;
+//    }
 }
